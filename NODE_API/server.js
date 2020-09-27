affb612b500656085d656394b19067aba1dfe254
dusanradivojevic <r.dusan97@gmail.com>
2020-09-28 01:07:11 +0200

    Changes needed for publishing on heroku

--- NODE_API/server.js
+++ NODE_API/server.js
@@ -23,14 +23,14 @@ app.use(cors());
 app.use(
   helmet({
     contentSecurityPolicy: false,
+    noSniff: false,
   })
 );
 app.use(requestID());
 
-app.use(express.static(path.join(__dirname, "../react-app/build")));
-
 // pokretanje react klijentske aplikacije
 app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
   res.sendFile(path.join(__dirname, "../react-app/build/index.html"));
 });
 
@@ -49,6 +49,10 @@ app.use("/api/prijemnica", prijemnicaRuter);
 // rutiranje zaposlenih
 const zaposleniRuter = require("./routes/zaposleni");
 app.use("/api/zaposleni", zaposleniRuter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../react-app/build")));
+}
 
 const port = process.env.PORT || 7150;
 app.listen(port, () => console.log(`Server started on port ${port}`));
